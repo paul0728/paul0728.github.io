fca19d479a940e80c0e954050f1a085ab14b9a49
paul0728 <44644609+paul0728@users.noreply.github.com>
2020-05-30 15:17:40 +0800

    Add files via upload

---
+++ web_project/test_site/scripts/main.js
@@ -0,0 +1,43 @@
+/*var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';*/
+
+// Image switcher code
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/f3.jpg');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
+// Personalized welcome message code
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+/*function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}*/
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
